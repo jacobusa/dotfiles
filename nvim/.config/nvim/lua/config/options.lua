de1f92780fc6b06a9bef4dff230820223da1992a
Kobus <jacobusbad@gmail.com>
2026-05-19 19:39:17 -0700

    changes

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,10 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
--- vim.opt.winbar = "%=%m %f"
--- vim.opt.termguicolors = true
--- vim.opt.cursorline = true
--- vim.opt.signcolumn = "yes"
--- vim.opt.number = true
--- vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.wrap = true
+
 vim.opt.clipboard = "unnamedplus"
