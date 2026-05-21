70279dfb0904daede67586b7401f0d1ffad3ccd8
Kobus <jacobusbad@gmail.com>
2026-05-20 22:54:39 -0700

    edit lsp

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -11,19 +11,29 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
+    -- dependencies = {
+    --   "jose-elias-alvarez/typescript.nvim",
+    --   init = function()
+    --     require("lazyvim.util").lsp.on_attach(function(_, buffer)
+    --       -- stylua: ignore
+    --       vim.keymap.set( "n", "<leader>co", "TypescriptOrganizeImports", { buffer = buffer, desc = "Organize Imports" })
+    --       vim.keymap.set("n", "<leader>cR", "TypescriptRenameFile", { desc = "Rename File", buffer = buffer })
+    --     end)
+    --   end,
+    -- },
     opts = {
-      servers = { eslint = {}, ts_ls = {} },
-      setup = {
-        -- eslint = function()
-        --   require("lazyvim.util").lsp.on_attach(function(client)
-        --     if client.name == "eslint" then
-        --       client.server_capabilities.documentFormattingProvider = true
-        --     elseif client.name == "tsserver" then
-        --       client.server_capabilities.documentFormattingProvider = false
-        --     end
-        --   end)
-        -- end,
-      },
+      servers = { eslint = {}, ts_ls = {}, pyright = {} },
+      -- setup = {
+      --   eslint = function()
+      --     require("lazyvim.util").lsp.on_attach(function(client)
+      --       if client.name == "eslint" then
+      --         client.server_capabilities.documentFormattingProvider = true
+      --       elseif client.name == "tsserver" then
+      --         client.server_capabilities.documentFormattingProvider = false
+      --       end
+      --     end)
+      --   end,
+      -- },
     },
   },
 }
