c4fa0ed0065b4b2a6ab8f24c955a4898a191ef73
Kobus <jacobusbad@gmail.com>
2026-05-26 12:16:32 -0700

    add onedark option for theme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -46,4 +46,14 @@ return {
       vim.cmd.colorscheme("kanagawa-dragon")
     end,
   },
+  {
+    "navarasu/onedark.nvim",
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require("onedark").setup({
+        style = "warm",
+      })
+      require("onedark").load()
+    end,
+  },
 }
