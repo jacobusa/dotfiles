876843140ce986c0f4b9f76eed94c8d2cf97c41f
Kobus <jacobusbad@gmail.com>
2026-05-26 00:07:56 -0700

    change plugins to extras

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -69,27 +69,4 @@ return {
       },
     },
   },
-  -- {
-  --   "nvim-mini/mini.diff",
-  --   event = "VeryLazy",
-  --   keys = {
-  --     {
-  --       "<leader>go",
-  --       function()
-  --         require("mini.diff").toggle_overlay(0)
-  --       end,
-  --       desc = "Toggle mini.diff overlay",
-  --     },
-  --   },
-  --   opts = {
-  --     view = {
-  --       style = "sign",
-  --       signs = {
-  --         add = "▎",
-  --         change = "▎",
-  --         delete = "",
-  --       },
-  --     },
-  --   },
-  -- },
 }
