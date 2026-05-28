cb4522b17b29eaf2c60f222b896ffc7160d2d866
Kobus <jacobusbad@gmail.com>
2026-05-27 18:07:43 -0700

    only include deno in mason

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -2,20 +2,38 @@ return {
   "mason-org/mason.nvim",
   opts = {
     ensure_installed = {
-      "stylua",
-      "shellcheck",
-      "shfmt",
-      "eslint-lsp",
-      "lua-language-server",
-      "markdown-toc",
-      "markdownlint-cli2",
-      "marksman",
-      "prettier",
-      "pyright",
-      "shellcheck",
-      "tree-sitter-cli",
-      "typescript-language-server",
       "deno",
     },
   },
 }
+
+-- Installed
+--   ◍ deno denols
+--   ◍ eslint-lsp eslint
+--   ◍ lua-language-server lua_ls
+--   ◍ markdown-toc
+--   ◍ markdownlint-cli2
+--   ◍ marksman
+--   ◍ prettier
+--   ◍ pyright
+--   ◍ shellcheck
+--   ◍ shfmt
+--   ◍ stylua
+--   ◍ tree-sitter-cli
+--   ◍ typescript-language-server ts_ls
+--
+--
+-- "stylua",
+-- "shellcheck",
+-- "shfmt",
+-- "eslint-lsp",
+-- "lua-language-server",
+-- "markdown-toc",
+-- "markdownlint-cli2",
+-- "marksman",
+-- "prettier",
+-- "pyright",
+-- "shellcheck",
+-- "tree-sitter-cli",
+-- "typescript-language-server",
+-- "deno",
