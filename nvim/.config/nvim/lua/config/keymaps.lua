3962124c9ab9692b04987992971a859c148c951d
Kobus <jacobusbad@gmail.com>
2026-06-10 08:27:56 -0700

    quality of life improvements

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,4 +2,4 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("i", "jk", "<Esc>", { noremap = false })
-vim.keymap.set("n", "gh", vim.lsp.buf.hover, { noremap = false })
+-- vim.keymap.set("n", "gh", vim.lsp.buf.hover, { noremap = false })
