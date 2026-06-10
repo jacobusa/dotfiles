032d3d133d3fb14ccf892c8ccc4c21aa7a9a86d5
Kobus <jacobusbad@gmail.com>
2026-06-10 08:01:03 -0700

    disable auto complete brackets

---
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,15 @@
+return {
+  "saghen/blink.cmp",
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    completion = {
+      accept = {
+        -- experimental auto-brackets support
+        auto_brackets = {
+          enabled = false,
+        },
+      },
+    },
+  },
+}
